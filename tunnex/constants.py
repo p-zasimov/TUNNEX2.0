66f9d3c9ca29005b7f472d4f7be25618c87f821b
Pavel Zasimov <zasimov.lhec@gmail.com>
2026-04-22 19:24:36 +0200

    Delete tunnex/constants.py

--- tunnex/constants.py
+++
@@ -1,5 +0,0 @@
-revelo = 1822.8886259874  # A unified atomic mass unit (dalton) in electron mass (atomic units)
-k_b = 3.166811563e-6  # Boltzmann constant in Hartree K-1
-light = 29979245800  # Speed of light in cm s-1
-cm_m1_to_Hartree = 1 / 219474.63136320  # cm-1 to Hartree
-Hartree_to_kJ_mol_m1 = 2625.49963947  # Hartree to kJ mol-1
