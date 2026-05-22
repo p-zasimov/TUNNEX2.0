8c8e4b06b0a78ec329ea919bacd4de7010478c11
Pavel Zasimov <zasimov.lhec@gmail.com>
2026-05-22 10:42:21 +0200

    Delete devtools directory

--- devtools/docs/user_guide.rst
+++
@@ -1,4 +0,0 @@
-User Guide
-===============
-
-This page details how to use TUNNEX2.0. 
--- devtools/tunnex_2/__main__.py
+++
@@ -1,4 +0,0 @@
-# tunnex_2/__main__.py
-import runpy
-
-runpy.run_path("tunnex_2/main.py", run_name="__main__")
